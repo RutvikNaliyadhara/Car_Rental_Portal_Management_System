0edb5e26ce74d98f5cedeca90155af4cb585a0ab
Rutvik Patel <naliyadhararutvik2005@gmail.com>
2026-07-23 22:46:20 +0530

    Final static files configuration

--- CarRentalPortal_Management_System/settings.py
+++ CarRentalPortal_Management_System/settings.py
@@ -110,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'carRental' / 'static']
 
