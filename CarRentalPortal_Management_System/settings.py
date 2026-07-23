a65b7a5e82aa5d89441dda51d344fa748f0830da
Rutvik Patel <naliyadhararutvik2005@gmail.com>
2026-07-23 22:35:46 +0530

    Added whitenoise to fix CSS and images

--- CarRentalPortal_Management_System/settings.py
+++ CarRentalPortal_Management_System/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -109,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carRental', 'static'),
